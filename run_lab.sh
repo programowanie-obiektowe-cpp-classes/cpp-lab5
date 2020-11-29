246e1322bbb9c4a4b479d6113610965e62e5ddea
Jakub Galecki <j.galecki@quickersim.com>
2020-11-29 19:10:50 +0100

    added scripts

---
+++ run_lab.sh
@@ -0,0 +1,4 @@
+clang-format -i --style=file lab5.cpp
+g++ -std=c++17 -Wall -Wextra -Wpedantic -Wshadow -o lab5.out lab5.cpp
+chmod +x lab5.out
+./lab5.out
---
+++ run_zal.sh
@@ -0,0 +1,5 @@
+mkdir -p zal/build
+cd zal/build
+cmake ..
+make -j2
+./zal
