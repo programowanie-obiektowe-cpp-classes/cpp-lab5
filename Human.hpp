ac234fbd49379da3885a1eac208832c0b343d85b
Jakub Gałecki <33153107+kubagalecki@users.noreply.github.com>
2022-12-06 23:05:58 +0100

    Human.hpp now follows member naming convention

--- Human.hpp
+++ Human.hpp
@@ -5,19 +5,19 @@
 class Human
 {
 public:
-    Human() : name{"Anonymous"}, age{0}, likes_dogs{true}, likes_cats{true} {}
-    Human(std::string name_, unsigned int age_, bool ld, bool lk)
-        : name{std::move(name_)}, age{age_}, likes_dogs{ld}, likes_cats{lk}
+    Human() : name_{"Anonymous"}, age_{0}, likes_dogs_{true}, likes_cats_{true} {}
+    Human(std::string name, unsigned int age, bool ld, bool lk)
+        : name_{std::move(name)}, age_{age}, likes_dogs_{ld}, likes_cats_{lk}
     {}
 
-    const std::string& getName() const { return name; }
-    unsigned int       getAge() const { return age; }
-    bool               isMonster() const { return !likes_dogs && !likes_cats; }
-    void               birthday() { ++age; }
+    const std::string& getName() const { return name_; }
+    unsigned int       getAge() const { return age_; }
+    bool               isMonster() const { return !likes_dogs_ && !likes_cats_; }
+    void               birthday() { ++age_; }
 
 private:
-    std::string  name;
-    unsigned int age;
-    bool         likes_dogs;
-    bool         likes_cats;
+    std::string  name_;
+    unsigned int age_;
+    bool         likes_dogs_;
+    bool         likes_cats_;
 };
